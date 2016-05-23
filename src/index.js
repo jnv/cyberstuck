943ede44d2d868a1b7323ea98bcd77fef1061216
Jan Vlnas <git@jan.vlnas.cz>
2016-05-23 22:14:01 +0200

    Wait for font load

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import './index.css'
 import WebFont from 'webfontloader'
 import Phaser from 'phaser'
 
-WebFont.load({
-  custom: {
-    families: ['PressStart2P'],
-  },
-})
-
 const game = new Phaser.Game(480, 640,
   Phaser.CANVAS, // renderer
   'game-container', // parent DOM ID
@@ -21,4 +15,10 @@ states.forEach(state => {
   game.state.add(state, require(`./states/${state}`).default)
 })
 
-game.state.start('Boot')
+WebFont.load({
+  custom: {
+    families: ['PressStart2P'],
+  },
+  timeout: 5000,
+  active: () => game.state.start('Boot'),
+})
