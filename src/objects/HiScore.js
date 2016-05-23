231906d86f6e2c884ac7db0c357672398d8ec617
Jan Vlnas <git@jan.vlnas.cz>
2016-05-23 22:14:20 +0200

    Prepare hiscore

---
+++ src/objects/HiScore.js
@@ -0,0 +1,18 @@
+import style from '../style'
+
+const data = [{
+  id: '1',
+  score: 9999,
+},
+]
+
+export default class PressButtonText extends Phaser.Group {
+  preload () {
+
+  }
+
+  constructor (game, parent) {
+    super(game)
+
+  }
+}
