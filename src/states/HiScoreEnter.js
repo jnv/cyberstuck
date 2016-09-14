5e4cd74d181f3f22a7df50964ddac2a6f5b57da7
Jan Vlnas <git@jan.vlnas.cz>
2016-09-14 11:18:32 +0200

    Prepare initials tweens

--- src/states/HiScoreEnter.js
+++ src/states/HiScoreEnter.js
@@ -39,24 +39,48 @@ export default class HiScoreEnter extends Phaser.State {
     const text = this.add.text(game.world.centerX, 50, TEXT, FONT_STYLE)
     text.anchor.set(0.5, 0)
 
-    const initials = ['A', 'A', 'A']
-    this.initials = initials
-    this.currentLetter = 0
+    this.currentLetter = -1
 
     const letters = []
+    const tweens = []
     const letterBox = 100
 
     const startPos = game.world.centerX - letterBox
 
     for (var i = 0; i < 3; i++) {
-      const letter = this.add.text(startPos + i * letterBox, game.world.centerY, initials[i], INITIALS_STYLE)
+      const letter = this.add.text(startPos + i * letterBox, game.world.centerY, 'A', INITIALS_STYLE)
       letter.anchor.set(0.5)
       letters[i] = letter
+      tweens[i] = this.add.tween(letter).to({alpha: 0}, 300, Phaser.Easing.Quadratic.Out, false, 0, -1, true)
     }
+
+    this.letters = letters
+    this.tweens = tweens
+
+    this.letterForward()
+  }
+
+  letterForward () {
+    if (this.currentLetter >= 0) {
+      // stop animating last letter
+      // set alpha to 1
+      this.tween[this.currentLetter].stop()
+      this.letters[this.currentLetter].alpha = 1
+    }
+
+    this.currentLetter++
+
+    if (this.currentLetter > this.letters.length) {
+      return
+    }
+
+    const letter = this.letters[this.currentLetter]
+    const tween = this.tweens[this.currentLetter]
+
+    tween.start()
   }
 
   update () {
-
   }
 }
 
