4bda2ab1b7592e384499ad9446a3afce6b12d66e
Jan Vlnas <git@jan.vlnas.cz>
2016-09-14 16:10:02 +0200

    Handle file storage manually

--- src/lib/storage.js
+++ src/lib/storage.js
@@ -1,17 +1,28 @@
 import path from 'path'
 import dataUriToBuffer from 'data-uri-to-buffer'
 import storage from 'electron-storage'
+import pify from 'pify'
+
+const fs = pify(require('fs'))
+const mkdirp = pify(require('mkdirp'))
 
 const app = require('electron').remote.app
 
-const BASE_DIR = app.getPath('userData')
+const BASE_DIR = path.join(app.getPath('userData'), 'data')
 const AVATARS_DIR = 'avatars'
+console.log(`Storage base dir is: ${BASE_DIR}`)
 
-export function saveDataUri (fileName, dataUri) {
-  const filePath = path.resolve(BASE_DIR, fileName)
+function saveFile (filePath, data) {
+  const fullPath = path.resolve(BASE_DIR, filePath)
+  const dir = path.dirname(fullPath)
+  return mkdirp(dir)
+          .then(() => fs.writeFile(fullPath, data))
+}
+
+export function saveDataUri (filePath, dataUri) {
   const buffer = dataUriToBuffer(dataUri)
   console.log(`Saving data to ${filePath}`)
-  return storage.set(filePath, buffer)
+  return saveFile(filePath, buffer)
 }
 
 function avatarPath (avatarId) {
