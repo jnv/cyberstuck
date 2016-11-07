3e745b4751a45750ce73bf2b3387e36312e34cad
Jan Vlnas <git@jan.vlnas.cz>
2016-11-07 01:30:26 +0100

    Fallback to console.log for extra methods

--- src/logger.js
+++ src/logger.js
@@ -1,25 +1,33 @@
 const {ipcRenderer} = require('electron')
 
+
+function objValues (obj) {
+  return Object.keys(obj).map(key => obj[key])
+}
+
 function ipcLog (method, args) {
-  const argsArr = Object.keys(args).map(key => args[key])
-  ipcRenderer.send('log', {method, args: argsArr})
+  ipcRenderer.send('log', {method, args: objValues(args)})
 }
 
 const logger = {}
 
+const consoleLog = console.log
 ;['profile', 'startTimer', 'verbose', 'info', 'warn', 'error']
   .forEach(method => {
     const originalMethod = console[method]
     // XXX function () is intentional to allow rebinding of this
     console[method] = function () {
       ipcLog(method, arguments)
-      return originalMethod.apply(window.console, arguments)
+      if (originalMethod) {
+        return originalMethod.apply(window.console, arguments)
+      } else {
+        return consoleLog.apply(window.console, arguments)
+      }
     }
     logger[method] = console[method]
   })
 
 // Handle console.log separately to set verbose level
-const consoleLog = console.log
 console.log = function () {
   ipcLog('verbose', arguments)
   return consoleLog.apply(window.console, arguments)
