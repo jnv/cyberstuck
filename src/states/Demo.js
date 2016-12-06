6bb6243fef4c088fea6de05bf44510085980cdcd
Jan Vlnas <git@jan.vlnas.cz>
2016-12-06 15:17:27 +0100

    Add DEMO blinking text to, well, demo

--- src/states/Demo.js
+++ src/states/Demo.js
@@ -1,6 +1,12 @@
+import style from '../style'
+
 let DEMO_ITER = 0
 const FILES_COUNT = 3
 const VIDEO_KEY = 'demoVideo'
+
+const DEMO_TEXT_STYLE = {
+  ...style.fontTitle,
+}
 
 function demoFileName (number) {
   return `assets/demo/${number}.ogv`
@@ -26,12 +32,16 @@ export default class Demo extends Phaser.State {
   create () {
     const {game, options} = this
 
-
     const video = game.add.video(VIDEO_KEY)
     video.addToWorld(game.world.centerX, game.world.centerY, 0.5, 0.5)
     video.play()
     video.onComplete.addOnce(this.onComplete, this)
     this.video = video
+
+    const demoText = this.add.text(game.world.centerX - 10, 400, 'DEMO', DEMO_TEXT_STYLE)
+    demoText.anchor.set(0.5, 0)
+    demoText.alpha = 0
+    this.add.tween(demoText).to({alpha: 1}, 500, Phaser.Easing.Exponential.Out, true, 0, -1, true)
 
     if (options.pressTextThunk) {
       options.pressTextThunk(game, this)
